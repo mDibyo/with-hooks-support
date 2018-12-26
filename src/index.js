ce746e74f34b4138b3aca0cba8d8f3a5a87e0733
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2018-12-26 21:05:21 +0400

    Set nice display names

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import invariant from 'tiny-invariant';
 
 function withHooksSupport(Component) {
+  const componentName = Component.displayName || Component.name;
+
   invariant(
     Component.prototype instanceof React.Component,
-    `${Component.name} must inherit from React.Component.`
+    `${componentName} must inherit from React.Component.`
   );
 
   class ComponentWithHookSupport extends Component {
+    static displayName = `withHooksSupport(${componentName})`;
+
     constructor(props) {
       super(props);
 
       this.RenderFn = super.render.bind(this);
+      this.RenderFn.displayName = `${componentName}.render`;
     }
 
     render() {
